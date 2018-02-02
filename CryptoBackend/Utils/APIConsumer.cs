26392d6a9e4c1ace75f27e48b83a8f39dbdd5263
H. Tuğkan Kibar <htkibar@gmail.com>
2018-02-03 02:05:50 +0300

    Added base url stubs

--- CryptoBackend/Utils/APIConsumer.cs
+++ CryptoBackend/Utils/APIConsumer.cs
@@ -8,6 +8,15 @@ namespace CryptoBackend.Utils
     class ApiConsumer
     {
         private static readonly HttpClient httpClient = new HttpClient();
+        public static string CEX_BASE_URL = "";
+        public static string BITFINEX_BASE_URL = "";
+        public static string BINANCE_BASE_URL = "";
+        public static string BITSTAMP_BASE_URL = "";
+        public static string BTCTURK_BASE_URL = "";
+        public static string COINBASE_BASE_URL = "";
+        public static string GEMINI_BASE_URL = "";
+        public static string KRAKEN_BASE_URL = "";
+
         public static Task<T> Get<T>(string requestUri) {
             var task = new Task<T>(() => {
                 var response = httpClient.GetAsync(requestUri).Result;
