ebc651c3a6c8f907280380db35815f48f7f39e12
H. Tuğkan Kibar <htkibar@gmail.com>
2018-02-05 14:23:07 +0300

    Updated coin models

--- CryptoBackend/Models/Coin.cs
+++ CryptoBackend/Models/Coin.cs
@@ -11,28 +11,22 @@ namespace CryptoBackend.Models
         private Guid id = Guid.Empty;
         private string name;
         private string symbol;
-        private decimal price;
         private decimal transferTimeMins;
-        private Fiat priceCurrency; // Make this fiat OR coin
 
         public Guid Id { get => id; set => id = value; }
         public string Name { get => name; set => name = value; }
         public string Symbol { get => symbol; set => symbol = value; }
-        public decimal Price { get => price; set => price = value; }
         public decimal TransferTimeMins { get => transferTimeMins; set => transferTimeMins = value; }
-        public Fiat PriceCurrency { get => priceCurrency; set => priceCurrency = value; }
 
         public static IEnumerable<Coin> Find(
             string name = null,
             string symbol = null
         ) {
-            // TODO: Retrieve price currency too
             var sql = @"
                 select
                 coin.id as Id,
                 coin.name as Name,
                 coin.symbol as Symbol,
-                coin.price as Price,
                 coin.transfer_time_mins as TransferTimeMins
                 from coins as coin
                 where 1=1
@@ -63,42 +57,32 @@ namespace CryptoBackend.Models
                         id,
                         name,
                         symbol,
-                        price,
                         transfer_time_mins,
-                        price_currency_id
                     )
                     values
                     (
                         @Id,
                         @Name,
                         @Symbol,
-                        @Price,
                         @TransferTimeMins,
-                        @PriceCurrencyId
                     )
                     returning id;
                 ", new {
                     Id = Guid.NewGuid(),
                     Name = name,
                     Symbol = symbol,
-                    Price = price,
-                    TransferTimeMins = transferTimeMins,
-                    PriceCurrencyId = priceCurrency.Id
+                    TransferTimeMins = transferTimeMins
                 });
             } else {
                 Database.Master.Run<Guid>(@"
                     update coins set
                     name=@Name,
                     symbol=@Symbol,
-                    price=@Price,
                     transfer_time_mins=@TransferTimeMins,
-                    price_currency_id=@PriceCurrencyId
                 ", new {
                     Name = name,
                     Symbol = symbol,
-                    Price = price,
-                    TransferTimeMins = transferTimeMins,
-                    PriceCurrencyId = priceCurrency.Id
+                    TransferTimeMins = transferTimeMins
                 });
             }
         }
---
+++ CryptoBackend/Models/CoinData.cs
@@ -0,0 +1,74 @@
+using System;
+using CryptoBackend.Utils;
+
+namespace CryptoBackend.Models
+{
+    public class CoinData
+    {
+        private Coin coin { get; set; }
+        private Exchange exchange { get; set; }
+        private DateTime updatedAt { get; set; }
+        private Fiat fiat { get; set; }
+        private decimal volume { get; set; }
+        private decimal high { get; set; }
+        private decimal low { get; set; }
+        private decimal ask { get; set; }
+        private decimal bid { get; set; }
+        private decimal lastPrice { get; set; }
+
+        public Coin Coin { get => coin; set => coin = value; }
+        public Exchange Exchange { get => exchange; set => exchange = value; }
+        public DateTime UpdatedAt { get => updatedAt; set => updatedAt = value; }
+        public Fiat Fiat { get => fiat; set => fiat = value; }
+        public decimal Volume { get => volume; set => volume = value; }
+        public decimal High { get => high; set => high = value; }
+        public decimal Low { get => low; set => low = value; }
+        public decimal Ask { get => ask; set => ask = value; }
+        public decimal Bid { get => bid; set => bid = value; }
+        public decimal LastPrice { get => lastPrice; set => lastPrice = value; }
+
+        public void Save()
+        {
+            Database.Master.Run<Guid>(@"
+                insert into coin_data
+                (
+                    coin_id,
+                    exchange_id,
+                    updated_at,
+                    volume,
+                    low,
+                    high,
+                    ask,
+                    bid,
+                    last_price,
+                    price_fiat_id
+                )
+                values
+                (
+                    @CoinId,
+                    @ExchangeId,
+                    @UpdatedAt,
+                    @Volume,
+                    @Low,
+                    @High,
+                    @Ask,
+                    @Bid,
+                    @LastPrice,
+                    @PriceFiatId
+                )
+                returning id;
+            ", new {
+                CoinId = Coin.Id,
+                ExchangeId = Exchange.Id,
+                UpdatedAt = DateTimeOffset.Now.ToUnixTimeMilliseconds(),
+                Volume = Volume,
+                Low = Low,
+                High = High,
+                Ask = Ask,
+                Bid = Bid,
+                LastPrice = LastPrice,
+                PriceFiatId = Fiat.Id
+            });
+        }
+    }
+}
